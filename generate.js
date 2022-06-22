c0f2edde2f2030f5a999da2c231190d5f776110d
bradrf <brad@bitpony.com>
2022-06-22 08:20:40 -0700

    try multiplatform release

---
+++ generate.js
@@ -0,0 +1,25 @@
+const fs = require('node:fs');
+const os = require('node:os');
+const path = require('node:path');
+const crypto = require('node:crypto');
+
+let ext;
+switch (os.platform()) {
+  case 'darwin':
+    ext = 'dmg';
+    break;
+  case 'linux':
+    ext = 'deb';
+    break;
+  case 'win32':
+    ext = 'exe';
+    break;
+  default:
+    console.error('unsupported platform:', os.platform());
+    process.exit(1)
+}
+
+if (!fs.existsSync('release')) fs.mkdirSync('release');
+
+const buf = crypto.randomBytes(60);
+fs.writeFileSync(path.join('release', `goofy.${ext}`), buf);

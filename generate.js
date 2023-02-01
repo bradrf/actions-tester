b2945439b17eb3cc10a5363c890906cab738c353
bradrf <brad@bitpony.com>
2023-02-01 13:23:53 -0800

    need manual trigger opt

--- generate.js
+++ generate.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 const crypto = require('node:crypto');
 
 console.log('platform:', os.platform());
-console.log('run:', 7);
+console.log('run:', 8);
 
 let ext;
 switch (os.platform()) {

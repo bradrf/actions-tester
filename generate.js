35bb2d63d887713ab2d9748676ec217e35b7b065
bradrf <brad@bitpony.com>
2023-01-31 17:18:26 -0800

    trigger

--- generate.js
+++ generate.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 const crypto = require('node:crypto');
 
 console.log('platform:', os.platform());
-console.log('run:', 3);
+console.log('run:', 4);
 
 let ext;
 switch (os.platform()) {

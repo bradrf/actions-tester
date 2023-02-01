0bee1a634903c02e2d2ec4d247d15d6d60f36b87
bradrf <brad@bitpony.com>
2023-02-01 13:17:18 -0800

    try triggering release manually

--- generate.js
+++ generate.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 const crypto = require('node:crypto');
 
 console.log('platform:', os.platform());
-console.log('run:', 5);
+console.log('run:', 6);
 
 let ext;
 switch (os.platform()) {

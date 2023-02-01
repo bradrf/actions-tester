0bd716e197b286eb408257c10fc8b92e2ac9df61
bradrf <brad@bitpony.com>
2023-02-01 13:20:54 -0800

    need secret

--- generate.js
+++ generate.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 const crypto = require('node:crypto');
 
 console.log('platform:', os.platform());
-console.log('run:', 6);
+console.log('run:', 7);
 
 let ext;
 switch (os.platform()) {
